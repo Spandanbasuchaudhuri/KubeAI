d86964d31feb07baba243cf9423a2c95fe0916bb
ArnavG <114428015+ArnavG-728@users.noreply.github.com>
2025-04-08 16:38:04 +0530

    Delete req_yaml directory

--- req_yaml/cleanup-stress.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-kubectl delete -f stress_files/ --namespace=monitoring --ignore-not-found
-echo "🧹 Stressors deleted from 'monitoring' namespace."
--- req_yaml/run-stress.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-kubectl apply -f stress_files/
-echo "✅ Stressors deployed in 'monitoring' namespace."
